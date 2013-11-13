3a6efd39d291ed9909eb27d92f2a4f84b22e86d1
Anthon Pang <apang@softwaredevelopment.ca>
2013-11-13 15:05:00 +0000

    remove composer.phar (refs vipsoft/code-coverage-common#3)

---
+++ tests/VIPSoft/CodeCoverageExtension/Compiler/DriverPassTest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Driver Compiler Pass
+ *
+ * @copyright 2013 Anthon Pang
+ * @license BSD-2-Clause
+ */
+
+namespace VIPSoft\CodeCoverageExtension\Compiler;
+
+use VIPSoft\TestCase;
+
+/**
+ * Driver compiler pass test
+ *
+ * @group Unit
+ */
+class DriverPassTest extends TestCase
+{
+}
---
+++ tests/VIPSoft/CodeCoverageExtension/Compiler/FilterPassTest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Filter Compiler Pass
+ *
+ * @copyright 2013 Anthon Pang
+ * @license BSD-2-Clause
+ */
+
+namespace VIPSoft\CodeCoverageExtension\Compiler;
+
+use VIPSoft\TestCase;
+
+/**
+ * Filter compiler pass test
+ *
+ * @group Unit
+ */
+class FilterPassTest extends TestCase
+{
+}
