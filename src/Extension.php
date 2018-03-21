9901e8142d7a8906b547b2f3f228ba301d0ab931
ek9 <dev@ek9.co>
2018-03-21 20:56:53 +0000

    Fix default suffix being 'src' (new value: '') fixes #28

--- src/Extension.php
+++ src/Extension.php
@@ -148,7 +148,7 @@ class Extension implements ExtensionInterface
                                            ->useAttributeAsKey('name')
                                            ->prototype('array')
                                                ->children()
-                                                   ->scalarNode('prefix')->defaultValue('src')->end()
+                                                   ->scalarNode('prefix')->defaultValue('')->end()
                                                    ->scalarNode('suffix')->defaultValue('.php')->end()
                                                ->end()
                                            ->end()
