861174f7b75f7a008a788498504337348692788f
Doug Wright <github@dougweb.org>
2020-06-29 23:12:11 +0100

    Test the coverage-generating paths of EventSubscriber

--- tests/EventSubscriberTest.php
+++ tests/EventSubscriberTest.php
@@ -5,14 +5,23 @@ declare(strict_types=1);
 namespace DVDoug\Behat\CodeCoverage\Test;
 
 use Behat\Behat\EventDispatcher\Event\ScenarioTested;
+use Behat\Gherkin\Node\FeatureNode;
+use Behat\Gherkin\Node\ScenarioNode;
 use Behat\Testwork\EventDispatcher\Event\ExerciseCompleted;
 use DVDoug\Behat\CodeCoverage\Service\ReportService;
 use DVDoug\Behat\CodeCoverage\Subscriber\EventSubscriber;
 use PHPUnit\Framework\TestCase;
+use Prophecy\PhpUnit\ProphecyTrait;
+use SebastianBergmann\CodeCoverage\CodeCoverage;
+use SebastianBergmann\CodeCoverage\Driver\Driver;
+use SebastianBergmann\CodeCoverage\Filter;
+use SebastianBergmann\CodeCoverage\RawCodeCoverageData;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 
 class EventSubscriberTest extends TestCase
 {
+    use ProphecyTrait;
+
     public function testCanSubscribeToEvents(): void
     {
         $eventSubscriber = new EventSubscriber(new ReportService([]));
@@ -52,5 +61,47 @@ class EventSubscriberTest extends TestCase
         $subscriber->afterExercise($event);
 
         self::assertTrue(true);
+    }
+
+    public function testScenarioWithCoverage(): void
+    {
+        $scenario = $this->createStub(ScenarioNode::class);
+        $scenario->method('getLine')->willReturn(123);
+
+        $feature = $this->createStub(FeatureNode::class);
+        $feature->method('getFile')->willReturn('/tmp/file');
+
+        $event = $this->createStub(ScenarioTested::class);
+        $event->method('getScenario')->willReturn($scenario);
+        $event->method('getFeature')->willReturn($feature);
+
+        $driver = $this->prophesize(Driver::class);
+        $driver->start()->shouldBeCalled();
+
+        $codeCoverage = new CodeCoverage($driver->reveal(), new Filter());
+
+        $subscriber = new EventSubscriber(new ReportService([]), $codeCoverage);
+        $subscriber->beforeScenario($event);
+
+        $driver->stop()->willReturn(RawCodeCoverageData::fromXdebugWithPathCoverage([]));
+        $driver->stop()->shouldBeCalled();
+
+        $subscriber = new EventSubscriber(new ReportService([]), $codeCoverage);
+        $subscriber->afterScenario($event);
+    }
+
+    public function testAfterExerciseWithCoverage(): void
+    {
+        $event = $this->createMock(ExerciseCompleted::class);
+
+        $driver = $this->prophesize(Driver::class);
+
+        $codeCoverage = new CodeCoverage($driver->reveal(), new Filter());
+
+        $reportService = $this->prophesize(ReportService::class);
+        $reportService->generateReport($codeCoverage)->shouldBeCalled();
+
+        $subscriber = new EventSubscriber($reportService->reveal(), $codeCoverage);
+        $subscriber->afterExercise($event);
     }
 }
